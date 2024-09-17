5d4b28054e436b972ba7db303dcf153530b2e44e
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-09-17 10:50:36 +0300

    update

--- db.sql
+++ db.sql
@@ -1,9 +1,9 @@
 
 CREATE TABLE actors (
     id SERIAL PRIMARY KEY,
-    firstName VARCHAR(255) NOT NULL,
-    lastName VARCHAR(255) NOT NULL,
-    dateOfBirth DATE NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -11,7 +11,7 @@ CREATE TABLE actors (
 CREATE TABLE movies (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    creationDate DATE NOT NULL,
-    actorId INTEGER NOT NULL,
+    creation_date DATE NOT NULL,
+    actor_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
